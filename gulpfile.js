099b4d9f68b5bfb4ddd4f04e36c0fdc0ca772d11
Dasalla, John Mathew <john.mathew.dasalla@accenture.com>
2025-07-23 10:12:01 +0800

    Change files

--- gulpfile.js
+++ gulpfile.js
@@ -6,14 +6,15 @@ const gulp = require("gulp"),
       concat = require("gulp-concat"),
       cleanCSS = require("gulp-clean-css"),
       uglify = require("gulp-uglify"),
-      sass = require("gulp-sass")(require("sass"));  // ✅ USE `sass` here
+      sass = require("gulp-sass")(require("sass")); // Add Sass support
 
 const paths = {
   webroot: "./Tailspin.SpaceGame.Web/wwwroot/",
-  scss: "./Tailspin.SpaceGame.Web/scss/**/*.scss",     // ✅ Add this line
-  cssDest: "./Tailspin.SpaceGame.Web/wwwroot/css"      // ✅ CSS output
+  scss: "./Tailspin.SpaceGame.Web/scss/**/*.scss",     // SCSS source folder
+  cssDest: "./Tailspin.SpaceGame.Web/wwwroot/css"      // Where to output compiled CSS
 };
 
+// Existing file paths
 paths.js = paths.webroot + "js/**/*.js";
 paths.minJs = paths.webroot + "js/**/*.min.js";
 paths.css = paths.webroot + "css/**/*.css";
@@ -21,17 +22,19 @@ paths.minCss = paths.webroot + "css/**/*.min.css";
 paths.concatJsDest = paths.webroot + "js/site.min.js";
 paths.concatCssDest = paths.webroot + "css/site.min.css";
 
-// ✅ Compile SCSS
-gulp.task("sass", function () {
+// Clean tasks
+gulp.task("clean:js", done => rimraf(paths.concatJsDest, done));
+gulp.task("clean:css", done => rimraf(paths.concatCssDest, done));
+gulp.task("clean", gulp.series(["clean:js", "clean:css"]));
+
+// Compile Sass to CSS
+gulp.task("sass", () => {
   return gulp.src(paths.scss)
     .pipe(sass().on("error", sass.logError))
     .pipe(gulp.dest(paths.cssDest));
 });
 
-gulp.task("clean:js", done => rimraf(paths.concatJsDest, done));
-gulp.task("clean:css", done => rimraf(paths.concatCssDest, done));
-gulp.task("clean", gulp.series(["clean:js", "clean:css"]));
-
+// Minify JS and CSS
 gulp.task("min:js", () => {
   return gulp.src([paths.js, "!" + paths.minJs], { base: "." })
     .pipe(concat(paths.concatJsDest))
@@ -46,5 +49,8 @@ gulp.task("min:css", () => {
     .pipe(gulp.dest("."));
 });
 
-gulp.task("min", gulp.series(["sass", "min:js", "min:css"])); // ✅ Add "sass" here
-gulp.task("default", gulp.series(["min"]));
+// Run Sass first, then minify
+gulp.task("min", gulp.series(["min:js", "min:css"]));
+
+// Default task
+gulp.task("default", gulp.series(["sass", "min"]));
