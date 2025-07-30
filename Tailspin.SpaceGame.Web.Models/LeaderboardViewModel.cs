66c2f0ad4a4b154081c97f1e2dc6f89dac1dec06
Dasalla, John Mathew <john.mathew.dasalla@accenture.com>
2025-07-30 12:12:27 +0800

    Fixes

--- Tailspin.SpaceGame.Web.Models/LeaderboardViewModel.cs
+++ Tailspin.SpaceGame.Web.Models/LeaderboardViewModel.cs
@@ -1,23 +1,18 @@
-using System.Collections.Generic;
-
-namespace TailSpin.SpaceGame.Web.Models
+public class LeaderboardViewModel
 {
-    public class LeaderboardViewModel
-    {
-        public IEnumerable<Score> Scores { get; set; }
-        public Profile PlayerProfile { get; set; }
+    public IEnumerable<Score> Scores { get; set; }
+    public Profile PlayerProfile { get; set; }
 
-        // Paging
-        public int Page { get; set; }
-        public int PageSize { get; set; }
-        public int TotalResults { get; set; }
-        public int PageCount => (int)System.Math.Ceiling((double)TotalResults / PageSize);
-        public string PrevLink { get; set; }
+    // Paging
+    public int Page { get; set; }
+    public int PageSize { get; set; }
+    public int TotalResults { get; set; }
+    public string PrevLink { get; set; }
+    public string NextLink { get; set; } //  Add this
 
-        // Game filtering
-        public string SelectedMode { get; set; }
-        public string SelectedRegion { get; set; }
-        public IEnumerable<string> GameModes { get; set; }
-        public IEnumerable<string> GameRegions { get; set; }
-    }
+    // Filters
+    public string SelectedMode { get; set; }
+    public string SelectedRegion { get; set; }
+    public IEnumerable<string> GameModes { get; set; }
+    public IEnumerable<string> GameRegions { get; set; }
 }
--- Tailspin.SpaceGame.Web.Models/Score.cs
+++ Tailspin.SpaceGame.Web.Models/Score.cs
@@ -1,12 +1,12 @@
-namespace TailSpin.SpaceGame.Web.Models
+public class Score : Modelto
 {
-    public class Score : Modelto
-    {
-        public string PlayerName { get; set; }
-        public int ScoreValue { get; set; } // rename to Score if view expects Score
-        public string GameMode { get; set; }
-        public string GameRegion { get; set; }
-        public string ProfileId { get; set; }
-        public bool HighScore { get; set; }
-    }
+    public string PlayerName { get; set; }
+    public int Score { get; set; }  // Match view expectation
+    public string GameMode { get; set; }
+    public string GameRegion { get; set; }
+    public string ProfileId { get; set; }
+    public bool HighScore { get; set; }
+
+    // ADD THIS:
+    public Profile Profile { get; set; }
 }
